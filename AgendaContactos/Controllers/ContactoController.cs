72710836e74a5aadde5aa68152e2beb76e5e7cd3
LEMUEL ACOSTA <LEMUEL ACOSTA@DESKTOP-52PIL0C>
2026-05-23 15:28:44 -0400

    Controladores de Evento y Contacto base listo

---
+++ AgendaContactos/Controllers/ContactoController.cs
@@ -0,0 +1,35 @@
+﻿using AgendaContactos.Models;
+using AgendaContactos.Models.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaContactos.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactoController : ControllerBase
+    {
+        private readonly AgendaDBContext _context;
+        public ContactoController(AgendaDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contacto>>> GetContactos()
+        {
+            return await _context.Contactos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Contacto>> GetContacto(Guid id)
+        {
+            var contacto = await _context.Contactos.FindAsync(id);
+            if (contacto == null)
+            {
+                return NotFound();
+            }
+            return contacto;
+        }
+    }
+}
---
+++ AgendaContactos/Controllers/EventoController.cs
@@ -0,0 +1,34 @@
+﻿using AgendaContactos.Models;
+using AgendaContactos.Models.Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AgendaContactos.Controllers
+{
+    public class EventoController : Controller
+    {
+        private readonly AgendaDBContext _context;  
+      
+        public EventoController(AgendaDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Evento>>> GetEventos()
+        {
+            return await _context.Eventos.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Evento>> GetEvento(Guid id)
+        {
+            var evento = await _context.Eventos.FindAsync(id);
+            if (evento == null)
+            {
+                return NotFound();
+            }
+            return evento;
+        }
+    }
+}
